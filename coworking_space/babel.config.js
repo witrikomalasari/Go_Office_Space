2a3b68fc6dce340dea707e3fb02c0f6fbb27177f
witrikomalasari <dede.witri@gmail.com>
2023-11-28 04:15:30 +0700

    add struktur folder and setting config eslintrc,babel,prettier,index,metro.config,add reactnative.config file

--- coworking_space/babel.config.js
+++ coworking_space/babel.config.js
@@ -1,3 +1,30 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./GoOfficeSpace'],
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.json',
+        ],
+        alias: {
+          tests: ['./tests/'],
+          '@components': './GoOfficeSpace/components',
+          '@screens': './GoOfficeSpace/screens',
+          '@navigators': './GoOfficeSpace/navigators',
+          '@assets': './GoOfficeSpace/assets',
+          '@utils': './GoOfficeSpace/utils',
+        },
+      },
+    ],
+  ],
 };
